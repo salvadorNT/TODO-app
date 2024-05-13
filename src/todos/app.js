08707a1325d4b69b623b2d436f70e9ac2f71cd16
salvador <salvadorjnt@gmail.com>
2024-05-13 11:20:00 -0600

    display Todos

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,4 +1,12 @@
 import html from "./app.html?raw";
+import todoStore from "../store/todo.store";
+import { renderTodos } from "./use-cases";
+
+
+const ElementIDs = {
+    TodosList: '.todo-list',
+}
+
 /**
  * 
  * @param {String} elementId 
@@ -6,10 +14,18 @@ import html from "./app.html?raw";
 export const App = (elementId) => {
 
 
+    const displayTodos = () => {
+        const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+        renderTodos(ElementIDs.TodosList, todos);
+
+    }
+
+
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
+        displayTodos();
     })();
 
 
